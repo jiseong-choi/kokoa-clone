decced04f4e7dcd94fe7ce297b2dfcbeb59bb6c4
jiseong-choi <jiseong1951@kakao.com>
2021-01-17 08:33:46 +0900

    add the alert time

--- clock.js
+++ clock.js
@@ -1,5 +1,6 @@
 const clockContainer = document.querySelector(".js-clock"),//html 에 접근 -> js-clock 클래스에 접근 아래도 같음. 
   clockTitle = clockContainer.querySelector("span");
+const alert = document.querySelector(".alert")
 
 function getTime() {
   const date = new Date();
@@ -12,8 +13,18 @@ function getTime() {
   //삼항 연산자란 앞에 두값을 비교한후 맞으면 : 앞에 값을 틀리면 뒤의 값을 넣어주는 식임;
 }
 
+function alterTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  alert.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`;
+}
+
 function init() {
-  setInterval(getTime, 100);//뒤에 인자는 리로드 시간 1은 1ms를 뜻함 fuction init는 기본 함수로 정의함;
+  alterTime();
+  setInterval(getTime, 1000);//뒤에 인자는 리로드 시간 1은 1ms를 뜻함 fuction init는 기본 함수로 정의함;
 }
 
 init();
